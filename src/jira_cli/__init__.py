08ab19025899aa30cfa60bd37dd268c8860dec21
Christopher Johnstone <chrisj97@proton.me>
2026-01-31 10:57:07 +0000

    Moved parser to own file

---
+++ src/jira_cli/__init__.py
@@ -0,0 +1,4 @@
+from .parser import main_parser
+
+
+__all__: list[str] = ["main_parser"]
---
+++ src/jira_cli/parser.py
@@ -0,0 +1,46 @@
+from argparse import ArgumentParser, RawTextHelpFormatter, _SubParsersAction
+
+from commands import COMMANDS, shared_parsers
+from constants import HelpCommands, HelpSubCommands
+
+
+def main_parser() -> ArgumentParser:
+    """
+    Create the main argument parser for the Jira CLI.
+
+    Returns:
+        ArgumentParser: The configured argument parser.
+    """
+    shared: list[ArgumentParser] = shared_parsers()
+    parser: ArgumentParser = ArgumentParser(
+        description="A Command Line Interface for Jira",
+        formatter_class=RawTextHelpFormatter,
+        parents=[*shared],
+    )
+    command_subs: _SubParsersAction = parser.add_subparsers(
+        title="command",
+        metavar="<command>",
+        required=True,
+    )
+    for command, command_module in COMMANDS.items():
+        command_parser: ArgumentParser = command_subs.add_parser(
+            name=command,
+            formatter_class=RawTextHelpFormatter,
+            parents=[*shared, *command_module.command_parsers()],
+            help=HelpCommands[command],
+            description=HelpCommands[command],
+        )
+        subcommand_subs: _SubParsersAction = command_parser.add_subparsers(
+            title="subcommand",
+            metavar="<subcommand>",
+            required=True,
+        )
+        for subcommand, subcommand_module in command_module.SUBCOMMANDS.items():
+            subcommand_subs.add_parser(
+                name=subcommand,
+                formatter_class=RawTextHelpFormatter,
+                parents=[*shared, *subcommand_module.command_parsers()],
+                help=HelpSubCommands[command][subcommand],
+                description=HelpSubCommands[command][subcommand],
+            )
+    return parser
