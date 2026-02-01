1fcb92b5ee68dd8b13ebfbf8c9ae7bea8d680667
Christopher Johnstone <chrisj97@proton.me>
2026-02-01 23:18:57 +0000

    Cleaned up tests

--- src/jira_cli/tests/test_cli.py
+++ src/jira_cli/tests/test_cli.py
@@ -13,13 +13,12 @@ from test_utils import TestSet, parametrize
 
 FILEPATH: Final[str] = "jira_cli.cli"
 COMMAND_PATH: Final[str] = "jira_cli.commands"
-COMMAND: Final[str] = "jira"
 
 
 main_cases: TestSet = {
     "board issues defaults": {
-        "expected_main": f"{COMMAND_PATH}.board.issues.main",
-        "argv": [COMMAND, "board", "issues", "--id", "123"],
+        "expected_main": "board.issues.main",
+        "argv": ["board", "issues", "--id", "123"],
         "expected_args": Namespace(
             config_path=DEFAULT_CONFIG_PATH,
             id=123,
@@ -29,9 +28,8 @@ main_cases: TestSet = {
         "expected_log_level": DEFAULT_LOG_LEVEL,
     },
     "board issues values": {
-        "expected_main": f"{COMMAND_PATH}.board.issues.main",
+        "expected_main": "board.issues.main",
         "argv": [
-            COMMAND,
             "board",
             "issues",
             "--id",
@@ -55,15 +53,14 @@ main_cases: TestSet = {
         "expected_log_level": 30,
     },
     "board list defaults": {
-        "expected_main": f"{COMMAND_PATH}.board.search.main",
-        "argv": [COMMAND, "board", "list"],
+        "expected_main": "board.search.main",
+        "argv": ["board", "list"],
         "expected_args": Namespace(config_path=DEFAULT_CONFIG_PATH, limit=10),
         "expected_log_level": DEFAULT_LOG_LEVEL,
     },
     "board list values": {
-        "expected_main": f"{COMMAND_PATH}.board.search.main",
+        "expected_main": "board.search.main",
         "argv": [
-            COMMAND,
             f"--{CONFIG_PATH_ARG_NAME}",
             "/custom/path",
             "board",
@@ -81,15 +78,14 @@ main_cases: TestSet = {
         "expected_log_level": 10,
     },
     "config show defaults": {
-        "expected_main": f"{COMMAND_PATH}.config.show.main",
-        "argv": [COMMAND, "config", "show"],
+        "expected_main": "config.show.main",
+        "argv": ["config", "show"],
         "expected_args": Namespace(config_path=DEFAULT_CONFIG_PATH),
         "expected_log_level": DEFAULT_LOG_LEVEL,
     },
     "config show values": {
-        "expected_main": f"{COMMAND_PATH}.config.show.main",
+        "expected_main": "config.show.main",
         "argv": [
-            COMMAND,
             "--log-level",
             "20",
             "config",
@@ -104,8 +100,8 @@ main_cases: TestSet = {
         "expected_log_level": 20,
     },
     "issue list defaults": {
-        "expected_main": f"{COMMAND_PATH}.issue.search.main",
-        "argv": [COMMAND, "issue", "list", "--jql", "assignee=currentUser()"],
+        "expected_main": "issue.search.main",
+        "argv": ["issue", "list", "--jql", "assignee=currentUser()"],
         "expected_args": Namespace(
             config_path=DEFAULT_CONFIG_PATH,
             jql="assignee=currentUser()",
@@ -114,9 +110,8 @@ main_cases: TestSet = {
         "expected_log_level": DEFAULT_LOG_LEVEL,
     },
     "issue list values": {
-        "expected_main": f"{COMMAND_PATH}.issue.search.main",
+        "expected_main": "issue.search.main",
         "argv": [
-            COMMAND,
             f"--{CONFIG_PATH_ARG_NAME}",
             "/home/user/jira_config.toml",
             "--log-level",
@@ -140,21 +135,25 @@ main_cases: TestSet = {
 
 
 @patch(f"{FILEPATH}.getLogger")
+@patch(f"{FILEPATH}.basicConfig")
+@patch("sys.argv")
 @parametrize(main_cases)
 def test_main(
+    mock_argv: MagicMock,
+    mock_basic_config: MagicMock,
     mock_get_logger: MagicMock,
     expected_main: str,
     argv: list[str],
     expected_args: Namespace,
     expected_log_level: int,
 ) -> None:
-    with (
-        patch("sys.argv", argv),
-        patch(expected_main) as mock_args_main,
-    ):
-        mock_logger: MagicMock = mock_get_logger.return_value
-        expected_args.main = mock_args_main
-        expected_args.logger = mock_logger
+    mock_argv.__getitem__.side_effect = ["", *argv].__getitem__
+    mock_logger: MagicMock = mock_get_logger.return_value
+    expected_args.logger = mock_logger
+    with patch(f"{COMMAND_PATH}.{expected_main}") as mock_args_main:
         main()
-        mock_args_main.assert_called_once_with(expected_args)
+    mock_basic_config.assert_called_once()
+    mock_get_logger.assert_called_once_with(FILEPATH)
     mock_logger.setLevel.assert_called_once_with(level=expected_log_level)
+    expected_args.main = mock_args_main
+    mock_args_main.assert_called_once_with(expected_args)
