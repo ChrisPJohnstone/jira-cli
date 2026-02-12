bbb393f23ece76086c75d49ee8450518af739d74
Christopher Johnstone <chrisj97@proton.me>
2026-02-12 21:12:48 +0000

    Added type checking to tests

--- src/jira_cli/config/type_definitions/config_raw.py
+++ src/jira_cli/config/type_definitions/config_raw.py
@@ -4,5 +4,6 @@ from typing import TypedDict
 class ConfigRaw(TypedDict):
     """TypedDict for raw configuration data."""
 
-    jira_base_url: str
     jira_api_token: str
+    jira_base_url: str
+    jira_email: str
--- src/jira_cli/jira_client/type_definitions/args_search_boards.py
+++ src/jira_cli/jira_client/type_definitions/args_search_boards.py
@@ -1,10 +1,12 @@
 from typing import TypedDict
 
+from jira_cli.jira_client import BoardType
+
 
 class ArgsSearchBoards(TypedDict, total=False):
     """Type definition for search boards arguments."""
 
-    board_type: str
+    board_type: BoardType
     name: str
     project: str
     limit: int
