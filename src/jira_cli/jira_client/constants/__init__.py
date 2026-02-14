84d012463585a0078244863d99eb2ec5a39746fe
Christopher Johnstone <chrisj97@proton.me>
2026-02-14 12:34:45 +0000

    Added validation arg to parse_jql

--- src/jira_cli/jira_client/constants/__init__.py
+++ src/jira_cli/jira_client/constants/__init__.py
@@ -1,12 +1,15 @@
-from .defaults import DEFAULT_PAGE_SIZE
+from .defaults import DEFAULT_PAGE_SIZE, DEFAULT_JQL_VALIDATION
 from .enum_api_version import APIVersion
 from .enum_board_type import BoardType
 from .enum_endpoint import Endpoint
+from .enum_jql_validation import JQLValidation
 
 
 __all__: list[str] = [
     "APIVersion",
     "BoardType",
     "DEFAULT_PAGE_SIZE",
+    "DEFAULT_JQL_VALIDATION",
     "Endpoint",
+    "JQLValidation",
 ]
--- src/jira_cli/jira_client/constants/defaults.py
+++ src/jira_cli/jira_client/constants/defaults.py
@@ -1,4 +1,7 @@
 from typing import Final
 
+from .enum_jql_validation import JQLValidation
+
 
 DEFAULT_PAGE_SIZE: Final[int] = 10
+DEFAULT_JQL_VALIDATION: Final[JQLValidation] = JQLValidation.STRICT
