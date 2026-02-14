60511f06b95dda1561619dcd45a28b39eb3223c8
Christopher Johnstone <chrisj97@proton.me>
2026-02-14 11:30:27 +0000

    Moved request construction to own method

--- src/jira_cli/jira_client/type_definitions/__init__.py
+++ src/jira_cli/jira_client/type_definitions/__init__.py
@@ -1,8 +1,10 @@
 from .args_search_board_issues import ArgsSearchBoardIssues
 from .args_search_boards import ArgsSearchBoards
+from .request_kwargs import RequestKwargs
 
 
 __all__: list[str] = [
     "ArgsSearchBoardIssues",
     "ArgsSearchBoards",
+    "RequestKwargs",
 ]
